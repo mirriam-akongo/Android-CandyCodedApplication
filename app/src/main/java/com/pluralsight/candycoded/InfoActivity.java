94033ea38475d46bc851729796f09fcc985705e6
mirriam-akongo <jemmirriako@gmail.com>
2019-11-02 00:09:02 +0300

    Starting google maps Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,9 +1,12 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -19,6 +22,24 @@ public class InfoActivity extends AppCompatActivity {
         Picasso.with(this).
                 load(uri).
                 into(candyStoreImageView);
+
+
+    }
+
+    public void createMapIntent(View view){
+
+        Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
+
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }else {
+            Toast.makeText(this,"No client is available", Toast.LENGTH_SHORT).show();
+        }
+
 
 
     }
